6a144335ac630f6e95ba6d8d009fbcc751938789
Kevin Dufour <47179566+Pardival@users.noreply.github.com>
2021-01-31 21:32:39 +0100

    Design de la page

--- script.js
+++ script.js
@@ -14,9 +14,9 @@
 
   /* De plus (a+bi)(a+bi) = a*a-b*b + 2*abi */
 
-  var iterationMax = 100;        // Reflète la qualité de notre fractale 
-  var width = 600;              // Longueur du canevas
-  var height = 450;             // Hauteur du canevas
+  var iterationMax = 200;        // Reflète la qualité de notre fractale 
+  var width = canvas.offsetWidth;              // Longueur du canevas
+  var height = canvas.offsetHeight;             // Hauteur du canevas
 
   /* Plan ou se trouve la fractale 
    * (Ce n'est pas celui di canevas il va donc falloir
@@ -30,28 +30,28 @@
   var yMax = 1.2;
   var longueurY = (yMax - yMin) 
 
-  /* ZOOM  sur le plan de la fractal afin que cell-ci soit proportionnel
+  /* ZOOM  sur le plan de la fractal afin que celle-ci soit proportionnel
    * au canevas 
   */
   var xZoom = width / longueurX;
   var yZoom = height / longueurY;
 
   /**
-   * Dessine une fractale
+   * Dessine un ensemble de Julia
+   * en fonction de sa consante complexe decomposé en deux partie 
+   * (une partie imaginaire et une partie réel) 
+   * @param c_reel partie réel
+   * @param c_imaginaire partie imaginaire
    */
-  function draw() {
-      /* Pour chaque point de coordonées (x,y) de mon canevas */
+  function drawJuliaSet(c_reel, c_imaginaire) {
+      console.log(c_imaginaire);
+      console.log(c_reel);
+    /* Pour chaque point de coordonées (x,y) de mon canevas */
     for (x = 0; x < width; x++) {
         for(y = 0; y < height; y++) {
-            /* Consante complexe decomposé en deux partie 
-            * (une partie imaginaire et une partie réel) 
-            */
-            var c_reel = -0.4 ;
-            var c_imaginaire = -0.6;
-
             /* Nombre complexe à l'itération 0 décomposé.
-            * Celui-ci est est ajusté au plan de la fractale 
-            * (une partie imaginaire et une partie réel) 
+            * Celui-ci est est ajusté au plan de la fractale. 
+            * on a une partie imaginaire et une partie réel
             */
             var z_reel = x / xZoom + xMin;
             var z_imaginaire = y / yZoom + yMin;
@@ -81,7 +81,55 @@
             * en fonction de où la boucle c'est areté 
             */
             } else {
-                var rgb = 'rgb(' + i * 255 / iterationMax + ',' + i * 255 / iterationMax + ',' + '0' + ')';
+                var rgb = 'rgb(' + i/2 * 255 / iterationMax + ',' + i * 255 / iterationMax + ',' + '55' + ')';
+                ctx.fillStyle = rgb;
+                ctx.fillRect(x, y, 1, 1);
+            }
+        }
+    }
+  }
+
+  function drawMandelbrotSet() {
+    /* Pour chaque point de coordonées (x,y) de mon canevas */
+    for (x = 0; x < width; x++) {
+        for(y = 0; y < height; y++) {
+
+            var c_reel = x / xZoom + xMin;
+            var c_imaginaire = y / yZoom + yMin;
+
+            /* Nombre complexe à l'itération 0 décomposé.
+            * Celui-ci est est ajusté au plan de la fractale. 
+            * on a une partie imaginaire et une partie réel
+            */
+            var z_reel = 0;
+            var z_imaginaire = 0;
+
+            /* */
+            var i = 0;
+
+            /* Tant que le module de z |z| ne semble pas tendre vers l'infini
+            * et que que nous avons pas atteint la qualité maximal de la fractal
+            * On procède à la suite Z -> Z^2 + C 
+            */
+            do {
+                var tempo = z_reel;     // pour faire le calcul de z_imaginaire à la bonne itération
+
+                z_reel = (z_reel * z_reel) - (z_imaginaire * z_imaginaire) + c_reel;
+                z_imaginaire = 2 * tempo * z_imaginaire + c_imaginaire;
+
+                i++;
+            } while ((z_reel * z_reel) + (z_imaginaire * z_imaginaire) < 4 
+                    && i < iterationMax);
+
+            /* Si la suite est borné alors on dessine le point de coordonées (x,y) */
+            if (i == iterationMax) {
+                ctx.fillStyle = "black";
+                ctx.fillRect(x, y, 1, 1);
+            /* Sinon on dessine un point d'une autre couleur 
+            * en fonction de où la boucle c'est areté 
+            */
+            } else {
+                var rgb = 'rgb(' + i/2 * 255 / iterationMax + ',' + i * 255 / iterationMax + ',' + '55' + ')';
                 ctx.fillStyle = rgb;
                 ctx.fillRect(x, y, 1, 1);
             }
@@ -89,4 +137,4 @@
     }
   }
  
-  draw();
+drawJuliaSet(-0.4, 0.6);
